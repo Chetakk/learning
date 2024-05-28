863c88f0f357d5aaa8f35ea891e6d4cb36a3afde
Chetakk <143086335+Chetakk@users.noreply.github.com>
2024-05-28 15:39:44 +0530

    matrix using list

--- python/matrix.py
+++ python/matrix.py
@@ -5,5 +5,18 @@ for i in range(len(mat1)):
     for j in range(len(mat2)):
         rmat[i][j]=mat1[i][j]+mat2[i][j]
 
+print("Added Matrix :-")
 for i in range(len(rmat)):
     print(rmat[i])
+
+#transpose
+
+tmat=[[0,0,0],[0,0,0],[0,0,0]]
+for i in range(len(rmat)):
+    for j in range(len(rmat)):
+        tmat[i][j]=rmat[j][i]
+
+print("\nTranpose :- ")
+
+for i in range(len(rmat)):
+    print(tmat[i])
