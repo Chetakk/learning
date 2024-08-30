c4813e8a94195e062403e4f5101352a985315136
Chetakk <143086335+Chetakk@users.noreply.github.com>
2024-08-30 20:25:22 +0530

    string

--- java/Bubblesort.java
+++ java/Bubblesort.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 public class Bubblesort{
     public static void main(String[] args){
         Scanner inp = new Scanner(System.in);
-        System.out.print("Enter the Number :");
+        System.out.print("Enter the number : ");
         int n = inp.nextInt();
-        int temp=0;
-        int arr[] = new int[n];
-        for(int i = 0; i < n ; i++){
-            arr[i]=inp.nextInt();
+        int a[] = new int[n];
+        for(int i = 0;i<n;i++){
+            System.out.printf("Enter element [%d] : ",i+1);
+            a[i] = inp.nextInt();
         }
-        for(int i = 0; i < n ; i++){
-            for(int j = i + 1; j < n; j++){
-                if(arr[j]<arr[i]){
-                    temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+        for(int i =0;i<n;i++)
+        {
+            for(int j = 0; j<n-i-1;j++){
+                if(a[j]>a[j+1]){
+                    int temp = a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
                 }
             }
         }
-        System.out.println("Sorted Array : ");
-        for(int i = 0; i < n ; i++){
-            System.out.print(arr[i]+" ");
+        System.out.println("Sorted Array :");
+        for(int i = 0;i<n;i++){
+            System.out.print(a[i]+" ");
         }
     }
 }
